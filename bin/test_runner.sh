03e543e162f2030056cfee660fff65180587f463
dueckes <matthew@ueckerman.net>
2016-12-08 10:27:08 +1100

    Servers are stopped post test run

---
+++ bin/test_runner.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+function clean_up() {
+  TESTS_EXIT_CODE=$?
+  echo ">>> Tests complete"
+
+  echo ">>> Stopping servers..."
+  npm run stop-servers
+  echo ">>> Servers stopped"
+
+  exit $TESTS_EXIT_CODE
+}
+
+echo ">>> Starting servers..."
+npm run start-servers
+echo ">>> Servers started"
+
+trap clean_up EXIT INT TERM
+
+echo ">>> Running tests..."
+npm run cucumber
