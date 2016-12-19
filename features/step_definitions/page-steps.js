dfb106e9699b306a5a7339b79e7f295e6bd322a7
dueckes <matthew@ueckerman.net>
2016-12-19 17:03:35 +1100

    Lint fix

--- features/step_definitions/page-steps.js
+++ features/step_definitions/page-steps.js
@@ -3,12 +3,12 @@ import World from '../support/world';
 module.exports = function () {
   this.World = World;
 
-  this.When(/^I navigate to the (.+) page$/, function(pageName) {
+  this.When(/^I navigate to the (.+) page$/, function (pageName) {
     this.page = this.pageRegistry.find(pageName);
     return this.page.loadAndWaitUntilVisible();
   });
 
-  this.Then(/^I am on the (.+) page$/, function(pageName) {
+  this.Then(/^I am on the (.+) page$/, function (pageName) {
     this.page = this.pageRegistry.find(pageName);
     return this.page.waitUntilVisible();
   });
