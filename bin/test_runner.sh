9a99da8d50e3b700bdadd66ee6c9e9f89401183f
Ryan Davis <me@rdavis.xyz>
2017-05-15 14:55:44 +1000

    Wait for the servers to respond before running tests

--- bin/test_runner.sh
+++ bin/test_runner.sh
@@ -13,8 +13,20 @@ function clean_up() {
   exit $TESTS_EXIT_CODE
 }
 
+function wait_until_responding() {
+  for i in $(seq 1 600); do
+    if nc -z $1 $2; then
+      break
+    else
+      sleep 0.1
+    fi
+  done
+}
+
 echo ">>> Starting servers..."
 yarn run start-servers
+wait_until_responding localhost 3000
+wait_until_responding localhost 5000
 echo ">>> Servers started"
 
 trap clean_up EXIT INT TERM
